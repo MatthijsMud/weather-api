361dda56354442e4e8b028e5d4222168318ffb37
Matthijs <matthijsmud@gmail.com>
2022-01-11 23:33:25 +0100

    Configured the app to use microservices.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({});
 
   const config = new DocumentBuilder()
     .setTitle("Weather")
-    .setDescription("")
+    .setDescription("API created as an assignment to assess skill level.")
     .setVersion("0.0.1")
     .addTag("weather")
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
