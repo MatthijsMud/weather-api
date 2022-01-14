609704684ea8231560d6cca5a87e756594877209
Matthijs <matthijsmud@gmail.com>
2022-01-14 17:01:00 +0100

    Import required services for the main module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from "@nestjs/schedule";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from "./tasks/tasks.module";
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(), 
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+    }),
+    TasksModule, 
+    CitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
