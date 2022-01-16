762c1238626dd269b635ca56d8e5e25191d7b6bf
Matthijs <matthijsmud@gmail.com>
2022-01-16 18:36:12 +0100

    Use the weather module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from "./tasks/tasks.module";
 import { CitiesModule } from './cities/cities.module';
+import { WeatherModule } from "./weather/weather.module";
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CitiesModule } from './cities/cities.module';
     }),
     TasksModule, 
     CitiesModule,
+    WeatherModule,
   ],
   controllers: [AppController],
   providers: [AppService],
