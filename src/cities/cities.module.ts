173b7c4e14377234542401fd8fc7ad560e627aba
Matthijs <matthijsmud@gmail.com>
2022-01-16 19:27:47 +0100

    Specify dependency on weather service in city.

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule } from "@nestjs/microservices";
 import { HttpModule } from "@nestjs/axios";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CitiesController } from './cities.controller';
@@ -6,7 +7,7 @@ import { CitiesService } from './cities.service';
 import { City } from "./city.entity";
 
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([City])],
+  imports: [HttpModule, TypeOrmModule.forFeature([City]), ClientsModule.register([{ name: "WEATHER_UPDATER_SERVICE" }])],
   controllers: [CitiesController],
   providers: [CitiesService],
 })
