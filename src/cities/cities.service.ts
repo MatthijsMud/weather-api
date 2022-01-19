a588a6f672bbba3f5223796296f76b3b1985f214
Matthijs <matthijsmud@gmail.com>
2022-01-19 16:53:17 +0100

    Insert the weather data into the database.

--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -4,9 +4,10 @@ import { ClientProxy }  from "@nestjs/microservices";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository, } from "typeorm";
 import { Observable, from, of, lastValueFrom, } from "rxjs";
-import { switchMap, concatAll, map, tap } from "rxjs/operators";
+import { switchMap, concatAll, map, tap, switchAll, } from "rxjs/operators";
 
 import { City } from "./city.entity";
+import { WeatherDto } from "../weather/dto/Weather.dto";
 
 @Injectable()
 export class CitiesService {
@@ -37,8 +38,8 @@ export class CitiesService {
     if (existing) {
       throw new HttpException("City already exists.", HttpStatus.CONFLICT);
     }
-
-    return this.city.save(this.city.create({ name }));
+    const weather = await lastValueFrom(this.client.send<WeatherDto>({ cmd: "retrieve" }, name));
+    return this.city.save({ weather, name });
   } 
 
   public async findOne(name: string): Promise<City> {
